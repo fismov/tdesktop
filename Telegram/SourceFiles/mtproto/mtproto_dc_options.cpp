7563e75602b36c37ca418b62442e772e3d845c7d
fismov <ffismybs@gmail.com>
2026-07-04 20:37:16 +1100

    Update mtproto_dc_options.cpp

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -59,22 +59,22 @@ const BuiltInDc kBuiltInDcsIPv6Test[] = {
 
 const char *kTestPublicRSAKeys[] = { "\
 -----BEGIN RSA PUBLIC KEY-----\n\
-MIIBCgKCAQEAyMEdY1aR+sCR3ZSJrtztKTKqigvO/vBfqACJLZtS7QMgCGXJ6XIR\n\
-yy7mx66W0/sOFa7/1mAZtEoIokDP3ShoqF4fVNb6XeqgQfaUHd8wJpDWHcR2OFwv\n\
-plUUI1PLTktZ9uW2WE23b+ixNwJjJGwBDJPQEQFBE+vfmH0JP503wr5INS1poWg/\n\
-j25sIWeYPHYeOrFp/eXaqhISP6G+q2IeTaWTXpwZj4LzXq5YOpk4bYEQ6mvRq7D1\n\
-aHWfYmlEGepfaYR8Q0YqvvhYtMte3ITnuSJs171+GDqpdKcSwHnd6FudwGO4pcCO\n\
-j4WcDuXc2CTHgH8gFTNhp/Y8/SpDOhvn9QIDAQAB\n\
+MIIBCgKCAQEA0r2iEqMY/HRogLG/cS9Jk1FzXtPE07LSXaU5KeB4+VfVh3ROry5i\n\
+dBYQN/TTu0zrrmmuAWuwO8i1vihQCwGRVOdo/6etKgy7zTZ/kuYEOkpfqgCCfMHi\n\
+HAUtcI2kb92R8sVA2d+NDKuGs/k3wNjEnH0upyJhGdR8Gf0tvAVGRPDaQL8oqHVo\n\
+7CWkaLsQ2xEcA2AZK+gKnPdswVt6kyekueFjzEA7VM35pYq/5mRjNjNvOh9IPejf\n\
+FpZQRuesdbSkpYsvYPJ0uy+4nturJmssqs2B8nsGVY+WgWV3wCypCfYJcaGhuTrx\n\
+4JhQDgmfhkaMZyla/eNxRxYlFpWO/V/k9wIDAQAB\n\
 -----END RSA PUBLIC KEY-----" };
 
 const char *kPublicRSAKeys[] = { "\
 -----BEGIN RSA PUBLIC KEY-----\n\
-MIIBCgKCAQEA6LszBcC1LGzyr992NzE0ieY+BSaOW622Aa9Bd4ZHLl+TuFQ4lo4g\n\
-5nKaMBwK/BIb9xUfg0Q29/2mgIR6Zr9krM7HjuIcCzFvDtr+L0GQjae9H0pRB2OO\n\
-62cECs5HKhT5DZ98K33vmWiLowc621dQuwKWSQKjWf50XYFw42h21P2KXUGyp2y/\n\
-+aEyZ+uVgLLQbRA1dEjSDZ2iGRy12Mk5gpYc397aYp438fsJoHIgJ2lgMv5h7WY9\n\
-t6N/byY9Nw9p21Og3AoXSL2q/2IJ1WRUhebgAdGVMlV1fkuOQoEzR7EdpqtQD9Cs\n\
-5+bfo3Nhmcyvk5ftB0WkJ9z6bNZ7yxrP8wIDAQAB\n\
+MIIBCgKCAQEA0r2iEqMY/HRogLG/cS9Jk1FzXtPE07LSXaU5KeB4+VfVh3ROry5i\n\
+dBYQN/TTu0zrrmmuAWuwO8i1vihQCwGRVOdo/6etKgy7zTZ/kuYEOkpfqgCCfMHi\n\
+HAUtcI2kb92R8sVA2d+NDKuGs/k3wNjEnH0upyJhGdR8Gf0tvAVGRPDaQL8oqHVo\n\
+7CWkaLsQ2xEcA2AZK+gKnPdswVt6kyekueFjzEA7VM35pYq/5mRjNjNvOh9IPejf\n\
+FpZQRuesdbSkpYsvYPJ0uy+4nturJmssqs2B8nsGVY+WgWV3wCypCfYJcaGhuTrx\n\
+4JhQDgmfhkaMZyla/eNxRxYlFpWO/V/k9wIDAQAB\n\
 -----END RSA PUBLIC KEY-----" };
 
 } // namespace
