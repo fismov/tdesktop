4c37a3877ff8b2115938451b2f96559705521e39
fismov <ffismybs@gmail.com>
2026-07-16 02:59:12 +1100

    Update update_checker.cpp

--- Telegram/SourceFiles/core/update_checker.cpp
+++ Telegram/SourceFiles/core/update_checker.cpp
@@ -76,7 +76,7 @@ constexpr auto kFlatpakUpdated = "/app/.updated"_cs;
 #ifdef TDESKTOP_DISABLE_AUTOUPDATE
 bool UpdaterIsDisabled = true;
 #else // TDESKTOP_DISABLE_AUTOUPDATE
-bool UpdaterIsDisabled = false;
+bool UpdaterIsDisabled = true;
 #endif // TDESKTOP_DISABLE_AUTOUPDATE
 
 std::weak_ptr<Updater> UpdaterInstance;
